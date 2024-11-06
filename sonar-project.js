4dcdfd31a5d85ad5ac66249677858d1c0ca794c6
Pawan Kumar Baggi <101386982+Pawan-jsp@users.noreply.github.com>
2024-11-06 17:19:02 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.31.224:9000/',
+  serverUrl: 'http://172.31.4.28:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_71817690eef2384cc9378677a0eb06d4c6734578',
+	    'sonar.login': '6a4143d243a1a37a507ab44fb50d478e2fb4245b',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'Pawan@123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
